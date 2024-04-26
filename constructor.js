a2d5d94034a0339b0d46c9785500df9a7763ffd5
kibaara-wangu <kibaarawangu@gmail.com>
2024-04-26 09:13:05 +0300

    add files

---
+++ constructor.js
@@ -0,0 +1,29 @@
+class Car {
+    constructor(make, model, year, isAvailable) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.isAvailable = isAvailable;
+    }
+    toggleAvailability() {
+        this.isAvailable = !this.isAvailable;
+    }
+}
+const wagon = new Car('limousine', 'volkswagen', 2020, 'true')
+console.log({wagon});
+// Rental class
+class Rental {
+    constructor(car, renterName, rentalStartDate, rentalEndDate) {
+        this.car = car;
+        this.renterName = renterName;
+        this.rentalStartDate = new Date(rentalStartDate);
+        this.rentalEndDate = new Date(rentalEndDate);
+    }
+    calculateRentalDuration() {
+        const  differenceInMilliseconds = this.rentalEndDate - this.rentalStartDate;
+        const differenceInDays = differenceInMilliseconds / (1000 * 60 * 60 * 24);
+        return differenceInDays;
+    }
+}
+const bus = new Rental ('volkswagen', 'Esther', "2024-4-26", "2024-6-26")
+console.log({bus});
